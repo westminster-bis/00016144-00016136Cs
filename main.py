c6c8d953e71bcce4e47184aaf8dce09f93a53629
00016136BIS <00016136wiutBis@gmail.com>
2023-11-14 16:09:17 +0500

    some methods added

--- main.py
+++ main.py
@@ -12,3 +12,42 @@ print(dataset.describe)
 # num_rows, num_columns = dataset.shape
 # print(f"Number of rows: {num_rows}")
 # print(f"Number of columns: {num_columns}")
+
+
+
+# List of items
+
+features = dataset.columns.tolist()
+print("Available features:")
+print(dataset.dtypes)
+
+sample_dataset = dataset.head(5)
+print(sample_dataset)
+
+# Data Cleaning
+
+school_Data = pd.DataFrame(dataset)
+school_Data.fillna('null')
+
+school_Data.dropna(inplace=True)
+print(school_Data)
+
+
+# Calculate Avarage
+
+def calculate_avarage(school_Data):
+    column = input('Enter column:')
+    avg = school_Data[column].mean()
+
+
+def calculate_min_max(school_Data):
+    column = input('Enter column:')
+    min = school_Data[column].min()
+    column = input('Enter column:')
+    max = school_Data[column].max()
+
+    return min, max
+
+
+print(f" Mode: {calculate_avarage(school_Data)}")
+print(f" Mean:{calculate_min_max(school_Data)}")
