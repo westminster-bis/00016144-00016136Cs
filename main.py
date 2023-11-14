58162745cdc4a5e76fb04260f75f1b0e5e2e3534
00016136BIS <150424124+00016136BIS@users.noreply.github.com>
2023-11-14 16:50:50 +0500

    my part

--- main.py
+++ main.py
@@ -13,7 +13,40 @@ print(dataset.describe)
 # print(f"Number of rows: {num_rows}")
 # print(f"Number of columns: {num_columns}")
 
-print("hello world")
-print("salom dunyo")
-print("salom dunyo")
-print("Mashxura's problem")
+
+# List of items
+
+features = dataset.columns.tolist()
+print("Available features:")
+print(dataset.dtypes)
+
+sample_dataset = dataset.head(5)
+print(sample_dataset)
+
+# Data Cleaning
+
+school_Data = pd.DataFrame(dataset)
+school_Data.fillna('null')
+
+school_Data.dropna(inplace=True)
+print(school_Data)
+
+
+# Calculate Avarage
+
+def calculate_avarage(school_Data):
+    column = input('Enter column:')
+    avg = school_Data[column].mean()
+
+
+def calculate_min_max(school_Data):
+    column = input('Enter column:')
+    min = school_Data[column].min()
+    column = input('Enter column:')
+    max = school_Data[column].max()
+
+    return min, max
+
+
+print(f" Mode: {calculate_avarage(school_Data)}")
+print(f" Mean:{calculate_min_max(school_Data)}")
