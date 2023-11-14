58f83d539d16525c5e939f55488063683637a925
wiutbis <148254115+wiutbis@users.noreply.github.com>
2023-11-14 13:23:06 +0500

    Data exploration

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-#this is testing
+import pandas as pd
+
+
+# Reading Dataset
+
+dataset = pd.read_csv('scores 2.csv')
+
+# Data Exploration
+
+print(dataset.describe)
+
+# num_rows, num_columns = dataset.shape
+# print(f"Number of rows: {num_rows}")
+# print(f"Number of columns: {num_columns}")
