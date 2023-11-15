70da47348ac0e5e0b669507fd2b1f9dc79f64f89
wiutbis <00016144bis@gmail.com>
2023-11-15 16:41:35 +0500

    removed some comments

--- main.py
+++ main.py
@@ -13,7 +13,3 @@ print(dataset.describe)
 # print(f"Number of rows: {num_rows}")
 # print(f"Number of columns: {num_columns}")
 
-print("hello world")
-print("salom dunyo")
-print("salom dunyo")
-print("Mashxura's problem")
